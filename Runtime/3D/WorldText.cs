751355bebbc806e1f75c76c9e4073842b9d4ddb3
RPGFabi <35190886+RPGFabi@users.noreply.github.com>
2024-01-29 15:25:36 +0100

    feat: implemented Gridsystem

---
+++ Runtime/3D/WorldText.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RPGFabi_Utils.World
+{
+    class WorldText
+    {
+        /// <summary>
+        /// Returns the Default Font by Unity
+        /// </summary>
+        /// <returns></returns>
+        public static Font GetDefaultFont()
+        {
+            return Resources.GetBuiltinResource<Font>("Arial.ttf");
+        }
+
+        public static TextMesh CreateWorldText(string text, Vector3 position, Quaternion rotation, Font font, int fontSize, Color color, TextAnchor anchor, TextAlignment alignment)
+        {
+            return CreateWorldText(null, text, position, rotation, font, fontSize, color, anchor, alignment);
+        }
+
+
+        /// <summary>
+        /// Creates an Text in the World
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="text"></param>
+        /// <param name="position"></param>
+        /// <param name="font"></param>
+        /// <param name="fonzSize"></param>
+        /// <param name="color"></param>
+        /// <param name="textAnchor"></param>
+        /// <param name="textAlignment"></param>
+        /// <returns></returns>
+        public static TextMesh CreateWorldText(Transform parent, string text, Vector3 position, Quaternion rotation, Font font, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment)
+        {
+            GameObject gameObject = new GameObject("World Text", typeof(TextMesh));
+            Transform transform = gameObject.transform;
+            transform.SetParent(parent);
+            transform.localPosition = position;
+            transform.localRotation = rotation;
+            TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+            textMesh.anchor = textAnchor;
+            textMesh.alignment = textAlignment;
+            textMesh.fontSize = fontSize;
+            textMesh.color = color;
+            textMesh.font = font;
+            textMesh.text = text;
+            return textMesh;
+        }
+    }
+}
---
+++ Runtime/Grid/Grid.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGFabi_Utils.World;
+
+namespace RPGFabi_Utils.Grid
+{
+    public class GridXY<GridObject>
+    {
+
+        [SerializeField] bool showDebug = false;
+
+        public float cellSize { get; private set; }
+        public Vector3 origin { get; private set; }
+        Dictionary<Vector2Int, GridObject> grid = new Dictionary<Vector2Int, GridObject>();
+        Dictionary<Vector2Int, TextMesh> debugTexts = new Dictionary<Vector2Int, TextMesh>();
+
+        public GridXY(float _cellSize, Vector3 _origin, Dictionary<Vector2Int,GridObject> defaultPositions = null)
+        {
+            float positiveCellSize = Mathf.Abs(_cellSize);
+            if(positiveCellSize > 0)
+            {
+                cellSize = positiveCellSize;
+            }
+            else
+            {
+                cellSize = 1f;
+            }
+
+            origin = _origin;
+
+            // Fill grid
+            if(defaultPositions != null)
+            {
+                foreach (var pos in defaultPositions)
+                {
+                    AddCellToGrid(pos.Key, pos.Value);
+                }
+            }
+
+        }
+
+        public void SetDebug(bool b)
+        {
+            showDebug = b;
+        }
+
+        public void AddCellToGrid(Vector2Int pos, GridObject obj)
+        {
+            if(grid.ContainsKey(pos))
+            {
+                Debug.LogWarning($"Grid already contains Cell at: {pos.x}:{pos.y}");
+                return;
+            }
+
+            grid.Add(pos, obj);
+            if(showDebug)
+            {
+                Debug_AddCellToDisplay(pos);
+            }
+        }
+
+        void Debug_AddCellToDisplay(Vector2Int pos)
+        {
+            Vector3 center = GetWorldPositionFromCell(pos);
+            debugTexts.Add(
+                pos,
+                WorldText.CreateWorldText(
+                    pos.ToString(),
+                    center,
+                    Quaternion.Euler(90,0,0),
+                    WorldText.GetDefaultFont(),
+                    20,
+                    Color.white,
+                    TextAnchor.MiddleCenter,
+                    TextAlignment.Center
+                    )
+                );
+
+            // Add Surroundings => Check if cell already drawn so no line needed
+            float halfCell = cellSize / 2f;
+            if(!grid.ContainsKey(pos + Vector2Int.left))
+            {
+                Debug.DrawLine(
+                    new Vector3(center.x - halfCell, center.y, center.z - halfCell),
+                    new Vector3(center.x - halfCell, center.y, center.z + halfCell),
+                    Color.white,
+                    100f
+                );
+            }
+            if (!grid.ContainsKey(pos + Vector2Int.right))
+            {
+                Debug.DrawLine(
+                    new Vector3(center.x + halfCell, center.y, center.z - halfCell),
+                    new Vector3(center.x + halfCell, center.y, center.z + halfCell),
+                    Color.white,
+                    100f
+                );
+            }
+            if (!grid.ContainsKey(pos + Vector2Int.up))
+            {
+                Debug.DrawLine(
+                    new Vector3(center.x - halfCell, center.y, center.z + halfCell),
+                    new Vector3(center.x + halfCell, center.y, center.z + halfCell),
+                    Color.white,
+                    100f
+                );
+            }
+            if (!grid.ContainsKey(pos + Vector2Int.down))
+            {
+                Debug.DrawLine(
+                    new Vector3(center.x - halfCell, center.y, center.z - halfCell),
+                    new Vector3(center.x + halfCell, center.y, center.z - halfCell),
+                    Color.white,
+                    100f
+                );
+            }
+        }
+
+
+        public Vector3 GetWorldPositionFromCell(Vector2Int index)
+        {
+            return origin + new Vector3(index.x,0,index.y)*cellSize;
+        }
+        public Vector2Int GetCellIndexFromWorld(Vector3 pos)
+        {
+            return new Vector2Int(
+                Mathf.FloorToInt((pos.x - origin.x - cellSize/2f) / cellSize),
+                Mathf.FloorToInt((pos.z - origin.z - cellSize/2f) / cellSize)
+            );
+        }
+
+        public GridObject GetCellObject(Vector2Int index)
+        {
+            if(grid.ContainsKey(index))
+            {
+                return grid[index];
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// Tries to place the gridobject in the cell - returns true if success
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool SetCellObject(Vector2Int index, GridObject obj)
+        {
+            if(grid.ContainsKey(index))
+            {
+                grid[index] = obj;
+                return true;
+            }
+            return false;
+        }
+    }
+}
